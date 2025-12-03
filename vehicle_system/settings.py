e9bab2cb45f6d98e7ddaa5a886dadda7bcdb6bc2
a7k4a6s3h0 <akashsuresh267@gmail.com>
2025-12-03 23:19:10 +0530

    static setting changed 2nd time

--- vehicle_system/settings.py
+++ vehicle_system/settings.py
@@ -137,6 +137,9 @@ STATIC_URL = '/static/'
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
